6d9fef3da2b8f52b17389b739c83866052244166
尼奥 <1988wangxiao@gmail.com>
2014-07-28 14:51:52 +0800

    [feature] 兼容植入控制代码。

--- player/main.js
+++ player/main.js
@@ -1,8 +1,8 @@
 void function() {
     window.wandoujia = window.wandoujia || {};
     window.wandoujia.audio = window.wandoujia.audio || {};
-    var audio = new Audio();
     var cover = document.querySelector('#player-cover');
+    var audio = document.querySelector('#player-audio');
 
     function extend(source, extendObj) {
         if (!source) {
@@ -16,8 +16,6 @@ void function() {
     extend(window.wandoujia.audio, {
         playerInit: function(opts) {
             audio.src = opts.audioSrc;
-            audio.style.opcity = 0;
-            document.body.appendChild(audio);
             cover.src = opts.imageSrc;
         }
     });
